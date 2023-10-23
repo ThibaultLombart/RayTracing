423cc4a66db9f709284c2e684f80c9a09f4c4f71
Thibault Lombart <lombart.thibault@gmail.com>
2023-10-23 09:40:38 +0200

    Ajout du Main - Début du projet

---
+++ src/main/Main.java
@@ -0,0 +1,25 @@
+/**
+ * Ce fichier fait partie du projet SAE RayTracing.
+ *
+ * (c) 2023 thibault.lombart
+ * Tous droits réservés.
+ */
+
+package main;
+
+
+/**
+ * Le type Main
+ *
+ * @author thibault.lombart
+ *
+ * @version 0.1.0
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
+
