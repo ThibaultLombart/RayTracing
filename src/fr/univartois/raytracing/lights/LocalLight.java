fca1b58008671539341bfbfae3aa6fd5211c01ad
Lucas Debloos <deblooslucas@gmail.com>
2023-10-24 08:18:08 +0200

    Ajout des commentaires dans les classes Light, LocalLight, DirectionalLight

--- src/fr/univartois/raytracing/lights/LocalLight.java
+++ src/fr/univartois/raytracing/lights/LocalLight.java
@@ -5,23 +5,46 @@ import fr.univartois.raytracing.digital.triples.Color;
 import fr.univartois.raytracing.digital.triples.Point;
 
 public class LocalLight extends Light{
-	
+	/**
+	 * Attribute point
+	 */
 	private Point point;
-	
+	/**
+	 * constructor LocalLight
+	 * @param color Color
+	 * @param point Point
+	 */
 	public LocalLight(Color color, Point point) {
 		super(color);
 		this.point = point;
 	}
-
-	public Point getVector() {
+	/**
+     * Get the point.
+     *
+     * @return Attribute point.
+       
+     */
+	public Point getPoint() {
 		return point;
 	}
 	
-	public void setVector(Point point) {
+	/**
+     * Modify attribute point
+     *
+     * @param point new value point.
+     */
+	public void setPoint(Point point) {
 		this.point = point;
 	}
-
-	public void setVector(double x, double y, double z) {
+	
+	/**
+     * Modify attribute point
+     *
+     * @param x new value x.
+     * @param y new value y.
+     * @param z new value z.
+     */
+	public void setPoint(double x, double y, double z) {
 		this.point = new Point(new Triplet(x,y,z));
 	}
 	
