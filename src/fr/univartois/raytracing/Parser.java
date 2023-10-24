428014aabc4cc62d3a496c83250a25950251c554
Bryan Lhomme <bryan_lhomme@ens.univ-artois.fr>
2023-10-24 10:30:01 +0200

    ✨ Parser V1

--- src/fr/univartois/raytracing/Parser.java
+++ src/fr/univartois/raytracing/Parser.java
@@ -1,106 +1,170 @@
 package fr.univartois.raytracing;
 
 import java.io.*;
+import java.util.List;
 import java.util.Scanner;
+
+import fr.univartois.raytracing.builder.ComplicatedObjectBuilder;
+import fr.univartois.raytracing.digital.triples.Color;
+import fr.univartois.raytracing.digital.triples.Point;
+import fr.univartois.raytracing.digital.triples.Vector;
+import fr.univartois.raytracing.lights.DirectionalLight;
+import fr.univartois.raytracing.lights.Light;
+import fr.univartois.raytracing.lights.LocalLight;
+import fr.univartois.raytracing.objects.Circle;
+import fr.univartois.raytracing.objects.Plane;
+import fr.univartois.raytracing.objects.Triangle;
 
 public class Parser {
 	
-	public static Scene lecture(String nomFichier){
-		Scene scene = new Scene();
+	public static ComplicatedObjectBuilder lecture(String nomFichier) throws Exception{
+		ComplicatedObjectBuilder scene = ComplicatedObjectBuilder.newInstance();
 		try {
 			BufferedReader reader = new BufferedReader(new FileReader(nomFichier));
 			String line;
+			
+			Scanner scanner;
 			while ((line = reader.readLine()) != null) {
-	                Scanner scanner = new Scanner(line);
+	                scanner = new Scanner(line);
+	                String token = null;
 	                if (scanner.hasNext()) {
-	                	String token = scanner.next().trim();
+	                	token = scanner.next().trim();
 	                }
 	                switch (token) {
 	                case "size":
 	                	if (scanner.hasNext()) {
-	                		scene.setSize(scanner.next().trim());
+	                		scene.setSizeX(Integer.parseInt(scanner.next().trim()));
+	                		scene.setSizeY(Integer.parseInt(scanner.next().trim()));
 	                	}
 			            break;
                     case "output":
 	                	if (scanner.hasNext()) {
-	                		scene.setOutput(scanner.next().trim());
+	                		scene.setName(scanner.next().trim());
 	                	}
 				        break;
 				    case "camera":
 	                	if (scanner.hasNext()) {
-	                		scene.setCamera(scanner.next().trim());
+	                		scene.setLookFrom(new Triplet(Double.parseDouble(scanner.next().trim()),Double.parseDouble(scanner.next().trim()),Double.parseDouble(scanner.next().trim())));
+	                		scene.setLookAt(new Triplet(Double.parseDouble(scanner.next().trim()),Double.parseDouble(scanner.next().trim()),Double.parseDouble(scanner.next().trim())));
+	                		scene.setUp(new Triplet(Double.parseDouble(scanner.next().trim()),Double.parseDouble(scanner.next().trim()),Double.parseDouble(scanner.next().trim())));
+	                		scene.setFov(Integer.parseInt(scanner.next().trim()));
 	                	}
 						break;
 					
 					case "ambient":
 	                	if (scanner.hasNext()) {
-	                		scene.setAmbient(scanner.next().trim());
+	                		scene.setAmbient(new Color(new Triplet(Double.parseDouble(scanner.next().trim()),Double.parseDouble(scanner.next().trim()),Double.parseDouble(scanner.next().trim()))));
 	                	}
 						break;
 							
 					case "diffuse" :
 	                	if (scanner.hasNext()) {
-	                		scene.setDiffuse(scanner.next().trim());
+	                		scene.setDiffuse(new Color(new Triplet(Double.parseDouble(scanner.next().trim()),Double.parseDouble(scanner.next().trim()),Double.parseDouble(scanner.next().trim()))));
 	                	}
 						break;
 	                
 					case "specular":
 	                	if (scanner.hasNext()) {
-	                		scene.setSize(scanner.next().trim());
+	                		scene.setSpecular(new Color(new Triplet(Double.parseDouble(scanner.next().trim()),Double.parseDouble(scanner.next().trim()),Double.parseDouble(scanner.next().trim()))));
 	                	}
 						break;
 	                	
 					case "shininess":
 	                	if (scanner.hasNext()) {
-	                		scene.setShininess(scanner.next().trim());
+	                		scene.setShininess(Integer.parseInt(scanner.next().trim()));
 	                	}
 						break;
 	                
 					case "directional":
 	                	if (scanner.hasNext()) {
-	                		scene.setDirectional(scanner.next().trim());
+	                		Triplet t1 = new Triplet(Double.parseDouble(scanner.next().trim()),Double.parseDouble(scanner.next().trim()),Double.parseDouble(scanner.next().trim()));
+	                		List<Light> l1 = scene.getLights();
+	                		double x = 0;
+	                		double y = 0;
+	                		double z = 0;
+	                		for( int i=0 ; i< l1.size() ; i++ ) {
+	                			x = x + l1.get(i).getColor().getTriplet().getX();
+	                			y = y + l1.get(i).getColor().getTriplet().getY();
+	                			z = z + l1.get(i).getColor().getTriplet().getZ();
+	                		}
+							if (x>1 || y>1 || z>1) {
+								scanner.close();
+								throw new Exception("Valeur(s) supérieure(s) à 1");
+							}
+							else {
+	                		scene.addLight(new DirectionalLight(new Color(t1),(new Vector(new Triplet(Double.parseDouble(scanner.next().trim()),Double.parseDouble(scanner.next().trim()),Double.parseDouble(scanner.next().trim()))))));
+							}
 	                	}
 						break;
 	                
 					case "point":
 	                	if (scanner.hasNext()) {
-	                		scene.setPoint(scanner.next().trim());
+	                		Triplet t1 = new Triplet(Double.parseDouble(scanner.next().trim()),Double.parseDouble(scanner.next().trim()),Double.parseDouble(scanner.next().trim()));
+	                		List<Light> l1 = scene.getLights();
+	                		double x = 0;
+	                		double y = 0;
+	                		double z = 0;
+	                		for( int i=0 ; i< l1.size() ; i++ ) {
+	                			x = x + l1.get(i).getColor().getTriplet().getX();
+	                			y = y + l1.get(i).getColor().getTriplet().getY();
+	                			z = z + l1.get(i).getColor().getTriplet().getZ();
+	                		}
+							if (x>1 || y>1 || z>1) {
+								scanner.close();
+								throw new Exception("Valeur(s) supérieure(s) à 1");
+							}
+							else {
+	                		scene.addLight(new LocalLight(new Color(t1),(new Point(new Triplet(Double.parseDouble(scanner.next().trim()),Double.parseDouble(scanner.next().trim()),Double.parseDouble(scanner.next().trim()))))));
+							}
 	                	}
 						break;
 	                
 					case "maxverts":
 			        	if (scanner.hasNext()) {
-			        		scene.setMaxverts(scanner.next().trim());
+			        		scene.setMaxVerts(Integer.parseInt(scanner.next().trim()));
 			        	}
 						break;
 	                
 					case "vertex":
 				    	if (scanner.hasNext()) {
-				    		scene.setVertex(scanner.next().trim());
+				    		scene.addPoint(new Point(new Triplet(Double.parseDouble(scanner.next().trim()),Double.parseDouble(scanner.next().trim()),Double.parseDouble(scanner.next().trim()))));
 				    	}
 						break;
 				    
 	
 					case "tri":
+						
 						if (scanner.hasNext()) {
-							scene.setTri(scanner.next().trim());
+							Color couleur = new Color(scene.getAmbient().getTriplet());
+							couleur = couleur.add(scene.getDiffuse());
+							if (couleur.getTriplet().getX()>1 || couleur.getTriplet().getY()>1 || couleur.getTriplet().getZ()>1) {
+								scanner.close();
+								throw new Exception("Valeur(s) supérieure(s) à 1");
+							}
+							else {
+							Point[] listePoints = {scene.getPoints().get(Integer.parseInt(scanner.next().trim())),scene.getPoints().get(Integer.parseInt(scanner.next().trim())),scene.getPoints().get(Integer.parseInt(scanner.next().trim()))};
+							scene.addShape(new Triangle(listePoints,couleur.add(scene.getDiffuse())));
+							} 
 						}
 						break;
 					
 					case "sphere":
 						if (scanner.hasNext()) {
-							scene.setSphere(scanner.next().trim());
+							Color couleur = new Color(scene.getAmbient().getTriplet());
+							scene.addShape(new Circle(new Point(new Triplet(Double.parseDouble(scanner.next().trim()),Double.parseDouble(scanner.next().trim()),Double.parseDouble(scanner.next().trim()))), Double.parseDouble(scanner.next().trim()) ,couleur.add(scene.getDiffuse())));
 						}
 						break;
 					
 					case "plane":
 						if (scanner.hasNext()) {
-							scene.setPlane(scanner.next().trim());
+							Color couleur = new Color(scene.getAmbient().getTriplet());
+							scene.addShape(new Plane(new Point(new Triplet(Double.parseDouble(scanner.next().trim()),Double.parseDouble(scanner.next().trim()),Double.parseDouble(scanner.next().trim()))), new Vector(new Triplet(Double.parseDouble(scanner.next().trim()),Double.parseDouble(scanner.next().trim()),Double.parseDouble(scanner.next().trim()))) ,couleur.add(scene.getDiffuse())));
 						}
 						break;
 					}
+	                scanner.close();
 	        }
-	        scanner.close();    
+	            
 	    }
 	    catch(IOException e){
 	      e.printStackTrace();
